968a7ae61f10433088a40af78b07e8ec794bd916
AshishNarwal <ashish.narwal50@gmail.com>
2019-03-22 10:05:03 +0530

    Add files via upload

---
+++ freelancer/src/main/java/com/cts/freelancer/bean/User.java
@@ -0,0 +1,84 @@
+
+	package com.cts.freelancer.bean;
+
+	import javax.persistence.Column;
+	import javax.persistence.Entity;
+	import javax.persistence.GeneratedValue;
+	import javax.persistence.GenerationType;
+	import javax.persistence.Id;
+import javax.persistence.Table;
+
+	@Entity
+	@Table
+	public class  User{
+	
+		private String firstName;
+		private String lastName;
+		private String emailId;
+		private String phoneNumber;
+		private String password;
+		@Id
+		@GeneratedValue(strategy=GenerationType.AUTO)
+		private int userId;
+
+	public User(){}
+
+	
+	public int getUserId() {
+		return userId;
+	}
+
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmailId() {
+		return emailId;
+	}
+
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+
+	
+	
+	
+	
+}
---
+++ freelancer/src/main/java/com/cts/freelancer/service/UserLoginServiceImpl.java
@@ -0,0 +1,28 @@
+package com.cts.freelancer.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.cts.freelancer.bean.User;
+import com.cts.freelancer.dao.UserLoginDAO;
+
+@Service("userLoginService")
+@Transactional(propagation=Propagation.SUPPORTS)
+public class UserLoginServiceImpl implements UserLoginService{
+
+	@Autowired
+	UserLoginDAO userLogin;
+
+	
+	public boolean authenticate(String emailId, String password) {
+		// TODO Auto-generated method stub
+		return userLogin.authenticate(emailId, password);
+	}
+
+	public boolean registerUser(User user) {
+		// TODO Auto-generated method stub
+		return userLogin.registerUser(user);
+	}
+}
